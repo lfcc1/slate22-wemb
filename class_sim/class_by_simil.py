7c5a4651b6a5f78b7a19bfacd23f3fa83f199b8c
lfcc1 <filipe-cunha1@hotmail.com>
2022-04-27 14:13:03 +0100

    classsificador por similaridade

---
+++ class_sim/class_by_simil.py
@@ -0,0 +1,90 @@
+from gensim.models import KeyedVectors
+import yaml
+from getopt import getopt
+import sys
+
+ops,args = getopt(sys.argv[1:],"vu")
+ops = dict(ops)
+
+
+model = KeyedVectors.load(args[0])
+
+f = open(args[1], encoding="utf8")
+sections= yaml.safe_load(f)
+
+
+score = 0
+oov = []
+length = 0
+
+
+def getTestsLength(sections):
+    count = 0
+    for section in sections:
+        count += len(section["testes"])
+    return count
+
+def createVerboseResult(sections):
+    result = []
+    for section in sections:
+        accepted = []
+        rejected = []
+        for test in section["testes"]:
+            try:
+                
+                accepted.append(test[0]) if test[2] == 1 else rejected.append(test[0])
+            except IndexError:
+                continue
+        result.append({"section":section["tit"],"score":section["score"],"accepted":accepted,"rejected":rejected})
+    return result
+
+def isUnknownWord(word):
+    if word not in model.key_to_index:
+        oov.append(word)
+        return True 
+    return False
+
+def containsUnknownWord(words):
+    for word in words:
+        if isUnknownWord(word):
+            return True
+    return False
+
+
+for section in sections:
+    if containsUnknownWord(section["clas"]):
+            continue
+    local_score = 0
+    local_length = 0
+    for test in section['testes']:
+        if isUnknownWord(test[0]):
+            continue
+        
+        local_length += 1
+        sim_result = [model.similarity(test[0],clas) for clas in section['clas']]
+        found_index = sim_result.index(max(sim_result))
+
+        #print(index,teste[0],elem['clas'][index])
+        if section['clas'][found_index] == test[1]:
+            local_score += 1
+            test.append(1)  ## Acepted
+        else:
+            test.append(0) ## Rejected
+
+    section["score"] = round(local_score/(local_length),4) if local_length != 0 else 0
+    score += local_score
+    length += local_length
+
+total_score = round(score/(length),4) if length != 0 else 0
+if '-v' in ops:
+    result = (total_score, createVerboseResult(sections))
+    print(result)
+
+if '-u' in ops:
+    real_length = getTestsLength(sections)
+    oov_ratio = round(len(oov)/real_length,4) if real_length != 0 else 0
+    print((oov_ratio,oov))
+
+else:
+    result = (total_score, [(section["tit"],section["score"]) for section in sections])     
+    print(result)
